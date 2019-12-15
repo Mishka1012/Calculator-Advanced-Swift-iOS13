f291abd623ac796bf3ff84c3c8b7293c0d7b0905
Mishka1012 <mikhailamshei@grodnoapps.com>
2019-12-14 19:57:02 -0600

    Simple Operations committed, change signing team to myself

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -11,11 +11,28 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
+    //this variable is only accessible within this class
+    private var isFinishedTypingNumber: Bool = true
     //all calculations
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        //calculation
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Non Number String Cast: \(displayLabel.text)")
+        }
+        guard let calcMethod = sender.currentTitle else {
+            fatalError("Calc button missing text")
+        }
+        if calcMethod == "+/-" {
+            displayLabel.text = String(number * -1)
+            //isFinishedTypingNumber = false
+        } else if calcMethod == "AC" {
+            displayLabel.text = "0"
+        } else if calcMethod == "%" {
+            displayLabel.text = String(number / 100)
+            //isFinishedTypingNumber = false
+        }
     }
     //all numbers
     @IBAction func numButtonPressed(_ sender: UIButton) {
@@ -23,7 +40,12 @@ class ViewController: UIViewController {
         guard let number = sender.currentTitle else {
             fatalError("Number button title returned nil!")
         }
-        displayLabel.text?.append(number)
+        guard isFinishedTypingNumber else {
+            displayLabel.text?.append(number)
+            return
+        }
+        isFinishedTypingNumber = false
+        displayLabel.text = number
     }
 }
 
